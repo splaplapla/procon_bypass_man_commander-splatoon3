66f08dbc240bafa573ccc400b16295db19b50682
jiikko <n905i.1214@gmail.com>
2023-04-10 08:19:03 +0900

    OK

--- lib/procon_bypass_man_commander/splatoon3/template_matcher.rb
+++ lib/procon_bypass_man_commander/splatoon3/template_matcher.rb
@@ -31,7 +31,6 @@ module ProconBypassManCommander
       # match_method = OpenCV::cv::TM_CCOEFF_NORMED
       # OpenCV::cv::match_template(edges_image, edges_template, result, match_method)
 
-
       def self.match(target_path:)
         first_template_path = './lib/procon_bypass_man_commander/splatoon3/assets/template-up-sd.png'
         second_template_path = './lib/procon_bypass_man_commander/splatoon3/assets/template-down-sd.png'
@@ -47,16 +46,14 @@ module ProconBypassManCommander
         OpenCV::cv::cvtColor(template, gray_template, OpenCV::cv::COLOR_BGR2GRAY)
         OpenCV::cv::cvtColor(second_template, gray_second_template, OpenCV::cv::COLOR_BGR2GRAY)
 
-        # 画面中央のみ検査する
         width = gray_image.cols
-        width_middle_start = (width / 7* 3).floor # 5分割
+        width_middle_start = (width / 7 * 3).floor
         width_middle_end = (width / 7 * 4).floor
         height = gray_image.rows
-        height_middle_start = (height / 3).floor # 3分割
+        height_middle_start = (height / 3).floor
         height_middle_end = (2 * height / 3).floor
         cutted_cropped_gray_image = gray_image.col_range(width_middle_start, width_middle_end)
         cropped_gray_image = cutted_cropped_gray_image.row_range(height_middle_start, height_middle_end)
-        OpenCV::cv::imwrite('hoge.png', cropped_gray_image) # debug
 
         result_cols = cropped_gray_image.cols - gray_template.cols + 1
         result_rows = cropped_gray_image.rows - gray_template.rows + 1
@@ -67,6 +64,7 @@ module ProconBypassManCommander
         min_location = OpenCV::cv::Point.new
         max_location = OpenCV::cv::Point.new
         min_val, max_val = OpenCV::cv::min_max_loc(result, min_location, max_location)
+
         if (match_method == OpenCV::cv::TM_SQDIFF || match_method == OpenCV::cv::TM_SQDIFF_NORMED) && min_val <= THRESHOLD
           match_location = min_location
         elsif max_val >= THRESHOLD
@@ -75,18 +73,13 @@ module ProconBypassManCommander
           return nil
         end
 
-        # NOTE: 判定結果をマーキング
-        match_location.x += width_middle_start # 正しい位置でマーキングするために検査対象を絞り込んだ部分を足す
+        # 画面中央で切り取った部分のオフセットを加える
+        match_location.x += width_middle_start
         match_location.y += height_middle_start
-
         OpenCV::cv::rectangle(image, match_location, OpenCV::cv::Point.new(match_location.x + template.cols, match_location.y + template.rows), OpenCV::cv::Scalar.new(0, 255, 0), 2, 8, 0)
-        result_image_path = "#{target_path}-result.png"
-        OpenCV::cv::imwrite(result_image_path, image)
+        OpenCV::cv::imwrite("#{target_path}-result.png", image)
 
-
-        # second_templateとのテンプレートマッチングを実行
         first_match_location = match_location.dup
-        # OpenCV::cv::imwrite('hoge.png', first_matched_and_cropped_gray_image) # debug
 
         result_cols = cropped_gray_image.cols - gray_second_template.cols + 1
         result_rows = cropped_gray_image.rows - gray_second_template.rows + 1
@@ -105,11 +98,14 @@ module ProconBypassManCommander
           return nil
         end
 
-        match_location.x += first_match_location.x
-        match_location.y += first_match_location.y
+
+        first_matching_x = match_location.x
+        first_matching_y = match_location.y
+        # first_templateのマッチング位置に対するオフセットを加える
+        match_location.x += width_middle_start
+        match_location.y += height_middle_start
         OpenCV::cv::rectangle(image, match_location, OpenCV::cv::Point.new(match_location.x + second_template.cols, match_location.y + second_template.rows), OpenCV::cv::Scalar.new(0, 255, 0), 2, 8, 0)
-        result_image_path = "#{target_path}-result.png"
-        OpenCV::cv::imwrite(result_image_path, image)
+        OpenCV::cv::imwrite("#{target_path}-result.png", image)
       end
     end
   end
