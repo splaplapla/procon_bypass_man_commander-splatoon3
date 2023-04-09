dfc47a43fbbc2b895db9fc437bb0d65474dd2cf2
jiikko <n905i.1214@gmail.com>
2023-04-10 06:26:18 +0900

    u

--- lib/procon_bypass_man_commander/splatoon3/template_matcher.rb
+++ lib/procon_bypass_man_commander/splatoon3/template_matcher.rb
@@ -3,6 +3,8 @@ require "procon_bypass_man_commander/splatoon3/version"
 module ProconBypassManCommander
   module Splatoon3
     class TemplateMatcher
+      THRESHOLD = 0.3
+
       # よくある前処理の実装例
       # image = OpenCV::cv::imread(target_path, OpenCV::cv::IMREAD_COLOR)
       # template = OpenCV::cv::imread(template_path, OpenCV::cv::IMREAD_COLOR)
@@ -29,27 +31,45 @@ module ProconBypassManCommander
       # OpenCV::cv::match_template(edges_image, edges_template, result, match_method)
 
 
-      def self.match(target_path:, template_path: './lib/procon_bypass_man_commander/splatoon3/assets/template-sd.png')
-        image = OpenCV::cv::imread(target_path, OpenCV::cv::IMREAD_COLOR)
-        template = OpenCV::cv::imread(template_path, OpenCV::cv::IMREAD_COLOR)
+      def self.match(target_path:, )
+        first_template_path = './lib/procon_bypass_man_commander/splatoon3/assets/template-up-sd.png'
 
-        result_cols = image.cols - template.cols + 1
-        result_rows = image.rows - template.rows + 1
+        image = OpenCV::cv::imread(target_path, OpenCV::cv::IMREAD_COLOR)
+        template = OpenCV::cv::imread(first_template_path, OpenCV::cv::IMREAD_COLOR)
+
+        gray_image = OpenCV::cv::Mat.new
+        gray_template = OpenCV::cv::Mat.new
+        OpenCV::cv::cvtColor(image, gray_image, OpenCV::cv::COLOR_BGR2GRAY)
+        OpenCV::cv::cvtColor(template, gray_template, OpenCV::cv::COLOR_BGR2GRAY)
+
+        # 画面中央のみ検査する
+        width = gray_image.cols
+        middle_start = (2 * width / 5).floor
+        middle_end = (3 * width / 5).floor
+        cropped_gray_image = gray_image.col_range(middle_start, middle_end)
+        OpenCV::cv::imwrite('hoge.png', cropped_gray_image)
+
+        result_cols = cropped_gray_image.cols - gray_template.cols + 1
+        result_rows = cropped_gray_image.rows - gray_template.rows + 1
         result = OpenCV::cv::Mat.new(result_rows, result_cols, OpenCV::cv::CV_32FC1)
         match_method = OpenCV::cv::TM_CCOEFF_NORMED
-        OpenCV::cv::match_template(image, template, result, match_method)
+        OpenCV::cv::match_template(cropped_gray_image, gray_template, result, match_method)
+
 
         # NOTE: 判定結果をマーキング
         min_loc = OpenCV::cv::Point.new
         max_loc = OpenCV::cv::Point.new
         min_val, max_val = OpenCV::cv::min_max_loc(result, min_loc, max_loc)
 
-        if match_method == OpenCV::cv::TM_SQDIFF || match_method == OpenCV::cv::TM_SQDIFF_NORMED
+        if (match_method == OpenCV::cv::TM_SQDIFF || match_method == OpenCV::cv::TM_SQDIFF_NORMED) && min_val <= THRESHOLD
           match_loc = min_loc
-        else
+        elsif max_val >= THRESHOLD
           match_loc = max_loc
+        else
+          return nil
         end
 
+        match_loc.x += middle_start
         OpenCV::cv::rectangle(image, match_loc, OpenCV::cv::Point.new(match_loc.x + template.cols, match_loc.y + template.rows), OpenCV::cv::Scalar.new(0, 255, 0), 2, 8, 0)
 
         result_image_path = "#{target_path}-result.png"
