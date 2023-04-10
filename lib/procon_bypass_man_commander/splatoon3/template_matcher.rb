cfcb6484a14a77b949000179d638cf8db466cf7e
jiikko <n905i.1214@gmail.com>
2023-04-11 00:12:03 +0900

    refactor

--- lib/procon_bypass_man_commander/splatoon3/template_matcher.rb
+++ lib/procon_bypass_man_commander/splatoon3/template_matcher.rb
@@ -36,35 +36,42 @@ module ProconBypassManCommander
         # 1回目のマッチング
         result_cols = cropped_gray_image.cols - gray_template.cols + 1
         result_rows = cropped_gray_image.rows - gray_template.rows + 1
-        result = OpenCV::cv::Mat.new(result_rows, result_cols, OpenCV::cv::CV_32FC1)
-        match_method = OpenCV::cv::TM_CCOEFF_NORMED
-        OpenCV::cv::match_template(cropped_gray_image, gray_template, result, match_method)
-
-        min_location = OpenCV::cv::Point.new
-        max_location = OpenCV::cv::Point.new
-        min_val, max_val = OpenCV::cv::min_max_loc(result, min_location, max_location)
-        if match_method == OpenCV::cv::TM_SQDIFF || match_method == OpenCV::cv::TM_SQDIFF_NORMED
-          first_matching_correlation_value = min_val
-          match_location = min_location
-        else
-          first_matching_correlation_value = max_val
-          match_location = max_location
-        end
-
+        matching_result = OpenCV::cv::Mat.new(result_rows, result_cols, OpenCV::cv::CV_32FC1)
+        OpenCV::cv::match_template(cropped_gray_image, gray_template, matching_result, match_method)
+        first_matching_correlation_value, match_location = calc_location(matching_result)
         first_matching_x = match_location.x
         first_matching_y = match_location.y
-        # 画面中央で切り取った部分のオフセットを加える
         match_location.x += width_middle_start
         match_location.y += height_middle_start
         OpenCV::cv::rectangle(image, match_location, OpenCV::cv::Point.new(match_location.x + first_template.cols, match_location.y + first_template.rows), OpenCV::cv::Scalar.new(0, 255, 0), 2, 8, 0) if debug
 
-
         # 2回目のマッチング
         result_cols = cropped_gray_image.cols - gray_second_template.cols + 1
         result_rows = cropped_gray_image.rows - gray_second_template.rows + 1
-        result = OpenCV::cv::Mat.new(result_rows, result_cols, OpenCV::cv::CV_32FC1)
-        OpenCV::cv::match_template(cropped_gray_image, gray_second_template, result, match_method)
+        matching_result = OpenCV::cv::Mat.new(result_rows, result_cols, OpenCV::cv::CV_32FC1)
+        OpenCV::cv::match_template(cropped_gray_image, gray_second_template, matching_result, match_method)
+        second_match_correlation_value, match_location = calc_location(matching_result)
+        second_matching_x = match_location.x
+        second_matching_y = match_location.y
+        match_location.x += width_middle_start
+        match_location.y += height_middle_start
 
+        if debug
+          OpenCV::cv::rectangle(image, match_location, OpenCV::cv::Point.new(match_location.x + second_template.cols, match_location.y + second_template.rows), OpenCV::cv::Scalar.new(0, 255, 0), 2, 8, 0)
+          OpenCV::cv::imwrite("#{target_path.gsub('.png', '')}-result.png", image)
+        end
+
+        # 3回目のマッチング
+        # TODO: 実装する
+
+        return [
+          MatchingResult.new(first_matching_x, first_matching_y, first_matching_correlation_value),
+          MatchingResult.new(second_matching_x, second_matching_y, second_match_correlation_value),
+        ]
+      end
+
+
+      def self.calc_location(result)
         min_location = OpenCV::cv::Point.new
         max_location = OpenCV::cv::Point.new
         min_val, max_val = OpenCV::cv::min_max_loc(result, min_location, max_location)
@@ -76,23 +83,11 @@ module ProconBypassManCommander
           match_location = max_location
         end
 
-        second_matching_x = match_location.x
-        second_matching_y = match_location.y
+        return [second_match_correlation_value, match_location]
+      end
 
-        match_location.x += width_middle_start
-        match_location.y += height_middle_start
-        OpenCV::cv::rectangle(image, match_location, OpenCV::cv::Point.new(match_location.x + second_template.cols, match_location.y + second_template.rows), OpenCV::cv::Scalar.new(0, 255, 0), 2, 8, 0) if debug
-        if debug
-          OpenCV::cv::imwrite("#{target_path.gsub('.png', '')}-result.png", image)
-        end
-
-        # 3回目のマッチング
-        # TODO: 実装する
-
-        return [
-          MatchingResult.new(first_matching_x, first_matching_y, first_matching_correlation_value),
-          MatchingResult.new(second_matching_x, second_matching_y, second_match_correlation_value),
-        ]
+      def self.match_method
+        OpenCV::cv::TM_CCOEFF_NORMED
       end
     end
   end
